4a2d9f27e5a26701dcac23de7f971e37f056d153
mahnaf <mahnaf125@gmail.com>
2025-07-13 00:29:52 +0600

    build: initialize server
    Initialize server with Express.js, MongoDB, CORS, DotEnv.

---
+++ index.js
@@ -0,0 +1,60 @@
+// Import SDKs
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv").config;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = 5100;
+dotenv();
+
+// Middlewares
+app.use(express.json());
+app.use(
+	cors({
+		origin: ["http://localhost:5173"],
+	}),
+);
+
+// Database URI
+const db_uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@professorcluster.rlegbqz.mongodb.net/?retryWrites=true&w=majority&appName=ProfessorCluster`;
+
+// Database Client
+const db_client = new MongoClient(db_uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+// Run APIs
+const run_api = async () => {
+	try {
+		// Client connection with server (turn off in deployment)
+		await db_client.connect();
+		// Define database
+		const db = db_client.db("tournext");
+		// Define collections
+		const usersColl = db.collection("users");
+		const toursColl = db.collection("tours");
+		// Ping for successful connection confirmation
+		await db_client.db("admin").command({ ping: 1 });
+		console.log("Pinged. Connected to MongoDB!");
+	} finally {
+		// Don't close client connection with server
+		// await client.close();
+	}
+};
+run_api().catch(console.dir);
+
+// Home route response
+app.get("/", (req, res) => {
+	res.send(
+		'<h1 style="font-family: sans-serif; text-align: center;">TourNext is travelling with streamlined guide</h1>',
+	);
+});
+
+// Listen to port
+app.listen(port, () => {
+	console.log(`Server is travelling on Port ${port}`);
+});
