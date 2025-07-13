25e99a6b40d349b72fc22d7ee680d69680f4a18c
mahnaf <mahnaf125@gmail.com>
2025-07-13 18:54:48 +0600

    feat(api): add DELETE /tours/:id API route
    Add DELETE API route in /tours/:id to delete a tour.

--- index.js
+++ index.js
@@ -69,6 +69,13 @@ const run_api = async () => {
 			const result = await toursColl.insertOne(newTour);
 			res.status(201).send(result);
 		});
+		// DELETE: Delete a tour
+		app.delete("/tours/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await toursColl.deleteOne(query);
+			res.status(204).send(result);
+		});
 		// Ping for successful connection confirmation
 		await db_client.db("admin").command({ ping: 1 });
 		console.log("Pinged. Connected to MongoDB!");
